32c00d7ca10aa45a518dc89bc8d5fab36f5430da
Zitronus <lmn.mdz1@gmail.com>
2026-06-25 18:01:04 +0800

    resett

--- shuttle-backend/roles/drivers.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-drivers_bp = Blueprint('drivers', __name__)
-
-# Dynamically assigned by app.py upon initialization
-supabase = None
-
-# 💡 Ready for your future driver custom updates! Add your new routes underneath this comment block.
--- shuttle-backend/roles/passenger.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-passenger_bp = Blueprint('passenger', __name__)
-
-# Dynamically assigned by app.py upon initialization
-supabase = None
-
-# 💡 Ready for your future passenger custom updates! Add your new routes underneath this comment block.
