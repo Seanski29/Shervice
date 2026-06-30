caa6fee6f1c74cf965dd564105aaade0f12b1d13
Seanski29 <seanmdelrosario@gmail.com>
2026-07-01 02:15:48 +0800

    For hotspot hosting

--- shervice_flutter/lib/constant.dart
+++ shervice_flutter/lib/constant.dart
@@ -1,12 +1,12 @@
-// lib/constants.dart
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 String get backendUrl {
   if (kIsWeb) return 'http://127.0.0.1:5000/api';
 
-  // This logic automatically switches between your phone's IP and local/emulator
-  return Platform.isAndroid
-      ? 'http://192.168.1.13:5000/api'
-      : 'http://127.0.0.1:5000/api';
+  if (Platform.isAndroid) {
+    return 'http://HOTSPOT:5000/api';
+  }
+  // Fallback for iOS Simulator or desktop
+  return 'http://127.0.0.1:5000/api';
 }
